e221e503ca4c0df09244d066c0f0b5a672fcf1f1
TzunderWulf <1006859@hr.nl>
2024-06-29 17:55:55 +0200

    added new component for a heat and cooling pump

---
+++ src/components/atoms/HeatCooling.tsx/HeatCooling.tsx
@@ -0,0 +1,14 @@
+
+import HeatCoolingProps from './HeatCooling.types';
+
+import React from "react";
+
+const HeatCooling: React.FC<HeatCoolingProps> = ({ xPos, yPos }) => {
+    return(
+        <div>
+
+        </div>
+    );
+};
+
+export default HeatCooling;
---
+++ src/components/atoms/HeatCooling.tsx/HeatCooling.types.ts
@@ -0,0 +1,4 @@
+export default interface HeatCoolingProps {
+  xPos: string,
+  yPos: string,
+}

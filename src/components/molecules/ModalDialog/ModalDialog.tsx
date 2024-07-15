7b21a2adf987c4fad1b5102455c230fc9bb89aaa
Sara van den Bos <svandenbos@tzunder.nl>
2024-07-15 14:15:38 +0200

    created folder structure for ModalDialog

---
+++ src/components/molecules/ModalDialog/ModalDialog.tsx
@@ -0,0 +1,13 @@
+
+import "./ModalDialog.scss";
+import ModalDialogProps from "./ModalDialog.types";
+
+const ModalDialog = () => {
+    return(
+        <div>
+
+        </div>
+    )
+};
+
+export default ModalDialog;

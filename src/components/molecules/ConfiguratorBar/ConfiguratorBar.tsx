ce301eb99cbc1555f59a46ff780649c5d3381380
TzunderWulf <1006859@hr.nl>
2024-07-16 23:33:51 +0200

    replaced text with icons

--- src/components/molecules/ConfiguratorBar/ConfiguratorBar.tsx
+++ src/components/molecules/ConfiguratorBar/ConfiguratorBar.tsx
@@ -6,9 +6,15 @@ const ConfiguratorBar: React.FC<ConfiguratorBarProps> = () => {
     <div className="configurator-bar">
       <div className="configurator-bar__background">
         <div className="configurator-bar__action-buttons">
-          <div className="configurator-bar__action configurator-bar__action__new-page">New page</div>
-          <div className="configurator-bar__action configurator-bar__action__save">Save</div>
-          <div className="configurator-bar__action configurator-bar__action__library">Library</div>
+          <div className="configurator-bar__action configurator-bar__action__new-page">
+            <img src="./icons/config/add.svg" />
+          </div>
+          <div className="configurator-bar__action configurator-bar__action__save">
+            <img src="./icons/config/save.svg" />
+          </div>
+          <div className="configurator-bar__action configurator-bar__action__library">
+            <img src="./icons/config/local_library.svg" />
+          </div>
         </div>
         <div>
           <p className="configurator-bar__mode-active">Currently in configuration mode</p>

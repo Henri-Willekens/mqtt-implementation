3d59f0d6abbfc6c22e72e8d781b7836204d1836c
TzunderWulf <1006859@hr.nl>
2024-07-16 18:45:45 +0200

    switched location to general for icons

--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -7,12 +7,12 @@ const Header: React.FC<HeaderProps> = ({ pages }) => {
   return (
     <div className='navigation'>
       <div className="navigation__block navigation__block-tabs">
-        <img className="navigation__block-icon" src="./icons/navigation.svg" alt="Tab navigation" />
+        <img className="navigation__block-icon" src="./icons/general/navigation.svg" alt="Tab navigation" />
       </div>
       <div className="navigation__block navigation__block-other">
-        <img className="navigation__block-icon" src="./icons/alerts.svg" alt="Alert tab" />
-        <img className="navigation__block-icon" src="./icons/account.svg" alt="Account" />
-        <img className="navigation__block-icon" src="./icons/settings.svg" alt="Settings" />
+        <img className="navigation__block-icon" src="./icons/general/alerts.svg" alt="Alert tab" />
+        <img className="navigation__block-icon" src="./icons/general/account.svg" alt="Account" />
+        <img className="navigation__block-icon" src="./icons/general/settings.svg" alt="Settings" />
       </div>
     </div>
   );

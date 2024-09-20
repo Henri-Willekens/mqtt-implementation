e4244d2f705d42b24cc3086fd7abda3055405958
TimWetzels <twetzels@tzunder.nl>
2024-09-20 10:56:42 +0200

    atempt to code modal

--- src/app/components/molecules/Header/Header.tsx
+++ src/app/components/molecules/Header/Header.tsx
@@ -1,12 +1,55 @@
 import HeaderProps from './Header.types';
 import './Header.scss';
 
+import { useState } from 'react';
 import Button from '../../atoms/Button/Button';
 import AlertBoxHeader from '../../atoms/AlertBoxHeader/AlertBoxHeader';
+import FormModal from '../FormModal/FormModal';
+import InputField from '../../atoms/FormInputs/InputField/InputField';
+import { Config } from 'src/app/configuration/types';
 
-const Header: React.FC<HeaderProps> = ({ pages, activePageId, navigateToPage }) => {
+const Header: React.FC<HeaderProps> = ({ pages, activePageId, configEnabled, navigateToPage }) => {
+  
+  const [_isModalOpen, setIsModalOpen] = useState(false);
+  const [_configData, setConfigData] = useState<Config>();
+  const [_formValues, setFormValues] = useState({
+  });
+  
   const switchToPage = (id: string) => {
     navigateToPage(id);
+  };
+
+  const openModal = () => {
+    if (configEnabled) {
+      setIsModalOpen(true);
+      fetch('/api/read-json')
+      .then((res) => res.json())
+      .then((results) => { 
+        setConfigData(results);
+      })
+      .catch((err) => console.error(err));
+    };
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    handleSave();
+  };
+
+  const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const _name = event.target.name;
+    const _value = event.target.value;
+
+    setFormValues((_prevFormValues) => ({
+      ..._prevFormValues,
+      [_name]: _value
+    }));
+  };
+
+  const handleSave = () => {
+    if (_configData === undefined) {
+      return;
+    }
   };
 
   const pageButtons = pages.map((_page) => {
@@ -21,6 +64,7 @@ const Header: React.FC<HeaderProps> = ({ pages, activePageId, navigateToPage }) 
     <div className='navigation'>
       <div className='navigation__block navigation__pages'>
         {pages.length < 10 ? pageButtons : <img src='./icons/general/apps.svg' className='navigation__pages-overview' onClick={() => switchToPage('PagesOverview')} />}
+        <Button value='+' onClick={openModal} />
       </div>
       <div className='navigation__block navigation__alerts'>
         <AlertBoxHeader type='alarm' status='unack' />
--- src/app/components/molecules/Header/Header.types.ts
+++ src/app/components/molecules/Header/Header.types.ts
@@ -3,5 +3,6 @@ import { PagesConfig } from "../../../configuration/types";
 export default interface HeaderProps {
   pages: PagesConfig[],
   navigateToPage: (id: string) => any,
-  activePageId: string
+  activePageId: string,
+  configEnabled: boolean
 }

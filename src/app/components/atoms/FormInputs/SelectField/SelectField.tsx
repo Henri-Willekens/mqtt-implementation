c972e4df9b2c06383b72861c6797e78933f8c2d0
Sara van den Bos <svandenbos@tzunder.nl>
2024-10-08 13:16:28 +0200

    added small bit of styling to selectfield

--- src/app/components/atoms/FormInputs/SelectField/SelectField.tsx
+++ src/app/components/atoms/FormInputs/SelectField/SelectField.tsx
@@ -1,3 +1,4 @@
+import './SelectField.scss'
 import SelectFieldProps from './SelectField.types';
 
 const SelectField: React.FC<SelectFieldProps> = ({
@@ -9,7 +10,7 @@ const SelectField: React.FC<SelectFieldProps> = ({
 }) => {
   return(
     <div className='select-field__field'>
-      <label htmlFor={id}>{label}</label>
+      <label className='select-field__label' htmlFor={id}>{label}</label>
       <select onChange={onChange} id={id} name={id} value={value} className='select-field__input'>
         {options.map((_option) => (
           <option key={_option} id={_option}>{_option}</option>

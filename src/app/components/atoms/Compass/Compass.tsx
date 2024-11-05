2a020fa1d0b8daa7639cfbcd1a5e324f4eca5771
Sara van den Bos <svandenbos@tzunder.nl>
2024-11-05 13:41:06 +0100

    Fixed issues with npm run build

--- src/app/components/atoms/Compass/Compass.tsx
+++ src/app/components/atoms/Compass/Compass.tsx
@@ -269,7 +269,13 @@ const Compass: React.FC<CompassProps> = ({
         <InputField label='Width (px)' type='number' id='width' value={formValues.width} onChange={handleChange} />
         <InputField label='Height (px)' type='number' id='height' value={formValues.height} onChange={handleChange} />
         <ToggleField label='Wave arrow outside?' id='waveArrowOutside' isChecked={stringToBool(formValues.waveArrowOutside.toString())} onChange={handleChange} />
-        <SelectField label='Datasource' id='dataSource' value={formValues.dataSource.toString()} options={['mqtt_topic', 'utc_time', 'local_time']} onChange={handleChange} />
+        <SelectField 
+          label='Datasource' 
+          id='dataSource' 
+          value={formValues.dataSource.toString()} 
+          options={[{label: 'MQTT topic', value: 'mqtt_topic'}, {label: 'UTC time', value: 'utc_time'}, {label: 'Local time', value: 'local_time'}]} 
+          onChange={handleChange} 
+        />
         {formValues.dataSource === 'mqtt_topic' && (
           <>
             <InputField type='text' label='MQTT Heading Topic' id='mqttTopic' value={formValues.mqttTopic} onChange={handleChange} />
